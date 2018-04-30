6f86104f02cce548dca7b46d4efec54f15c53051
Alexander Kallaway <kallaway@me.com>
2018-04-29 21:23:03 -0400

    add multiple routes, add displaying JSON from Go code

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+// ToDo represents a possible action - either done or not
 type ToDo struct {
-	Task string
-	Done bool
+	Task string `json:"task"`
+	Done bool   `json:"done"`
 }
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
+func serveTodos(w http.ResponseWriter, r *http.Request) {
 	jsonData, err := ioutil.ReadFile("data/todos.json")
 	if err != nil {
 		fmt.Println("Error reading file")
@@ -22,9 +24,36 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(jsonData))
 }
 
-func main() {
-	http.HandleFunc("/", sayHello)
+func serveAPIRules(w http.ResponseWriter, r *http.Request) {
+	apiRules := `
+		Here are the available API endpoints
+		/todos - view the list of todos (served as JSON)
+		/todos-from-data - view the list of todos generated from Go code
+	`
+	w.Write([]byte(apiRules))
+}
 
+func serveTodosFromData(w http.ResponseWriter, r *http.Request) {
+	todoSlice := []ToDo{
+		ToDo{Task: "walk the dog", Done: false},
+		ToDo{Task: "wash the dishes", Done: true},
+		ToDo{Task: "write in journal", Done: false},
+	}
+
+	todosJSON, err := json.Marshal(todoSlice)
+	if err != nil {
+		fmt.Errorf("Error creating JSON: %v", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(todosJSON))
+}
+
+func main() {
+	http.HandleFunc("/todos", serveTodos)
+	http.HandleFunc("/todos-from-data", serveTodosFromData)
+	http.HandleFunc("/", serveAPIRules)
+	fmt.Println("The server is now available at port :8100")
 	if err := http.ListenAndServe(":8100", nil); err != nil {
 		panic(err)
 	}
